a4965e761c137c06f5154f482d7524d67d05ee68
Pain <syopain@foxmail.com>
2020-09-12 18:28:14 +0800

    first week

---
+++ include/vector.h
@@ -0,0 +1,220 @@
+﻿#ifndef VECTOR_H
+#define VECTOR_H
+#include <initializer_list>
+#include <string>
+#include <stdexcept>
+
+namespace syo {
+
+    using std::initializer_list;
+    using std::string;
+
+    template <typename T> class Vector {
+    public:
+        using size_type = unsigned long long;
+        static const size_type MINSIZE = 16;
+
+        Vector() = default;
+        Vector(size_type size, const T &val = T{});
+        Vector(const Vector<T> &from);
+        Vector(initializer_list<T> il);
+        Vector& operator=(const Vector<T> &from);
+        ~Vector();
+
+        T& operator[](size_type index) { return *(p_arr_+ index ); }
+        const T& operator[](size_type index) const { return *(p_arr_ + index); }
+        T& at(size_type loc);
+        const T& at(size_type loc) const;
+        T& back();
+        const T& back() const;
+        size_type capacity() const { return capacity_; }
+        void clear() { size_ = 0; }
+        bool empty() const { return size_ == 0; }
+        T& front();
+        const T& front() const;
+        size_type size() const { return size_; }
+
+        void push_back(const T &val);
+        void pop_back();
+        void insert(size_type index, size_type num, const T &val);
+        void remove(size_type index, size_type num);
+        void reserve( size_type size);
+
+    private:
+        T *p_arr_ = nullptr;
+        size_type size_ = 0;
+        size_type capacity_ = 0;
+
+        void expand_capacity(int multiple);
+        void check(size_type i, const string &msg) const
+        {
+            if(i < 0 || i >= size_) throw std::out_of_range(msg);
+        }
+        void copy(const Vector<T> &from)
+        {
+            delete [] p_arr_;
+            p_arr_ = new T[from.capacity()] {};
+            for (size_type i = 0; i < from.size(); ++i) {
+                p_arr_[i] = from.p_arr_[i];
+            }
+        }
+    };
+
+    template <typename T>
+    Vector<T>::Vector(size_type size, const T &val) :
+        size_(size),
+        capacity_(size)
+    {
+        if (size > 0) {
+            p_arr_ = new T[capacity_] {};
+            if(val != T{})
+                for(size_type i = 0; i < size_; ++i) {
+                    p_arr_[i] = val;
+                }
+        }
+    }
+
+    template <typename T>
+    Vector<T>::Vector(const Vector<T> &from) :
+        size_(from.size_),
+        capacity_(from.capacity_)
+    {
+        copy(from);
+    }
+
+    template <typename T>
+    Vector<T>::Vector(initializer_list<T> il) :
+        size_(il.size()),
+        capacity_(il.size())
+    {
+        p_arr_ = new T[capacity_] {};
+        auto p = p_arr_;
+        for (const auto &elem : il) {
+            *p++ = elem;
+        }
+    }
+
+    template <typename T>
+    Vector<T>::~Vector()
+    {
+        delete [] p_arr_;
+        p_arr_ = nullptr;
+    }
+
+    template <typename T>
+    Vector<T>& Vector<T>::operator=(const Vector<T> &from)
+    {
+        if (p_arr_ != from.p_arr_) {
+            size_ = from.size_;
+            capacity_ = from.capacity_;
+            copy(from);
+        }
+        return *this;
+    }
+
+    template <typename T>
+    T& Vector<T>::at(size_type index)
+    {
+        check(index, "range of out on at()!");
+        return p_arr_[index];
+    }
+
+    template <typename T>
+    const T& Vector<T>::at(size_type index) const
+    {
+        check(index, "range of out on at()!");
+        return p_arr_[index];
+    }
+
+    template <typename T>
+    T& Vector<T>::front()
+    {
+        check(0, "front() on empty Vector!");
+        return p_arr_[0];
+    }
+
+    template <typename T>
+    const T& Vector<T>::front() const {
+        check(0, "front() on empty Vector!");
+        return p_arr_[0];
+    }
+
+    template <typename T>
+    T& Vector<T>::back()
+    {
+        check(0, "back() on empty Vector!");
+        return p_arr_[size_ - 1];
+    }
+
+    template <typename T>
+    const T& Vector<T>::back() const
+    {
+        check(0, "back() on empty Vector!");
+        return p_arr_[size_ - 1];
+    }
+
+    template <typename T>
+    void Vector<T>::push_back(const T &val)
+    {
+        reserve(size() + 1);
+        p_arr_[size_++] = val;
+    }
+
+    template <typename T>
+    void Vector<T>::pop_back()
+    {
+        check(0, "pop_back() on empty Vector!");
+        --size_;
+    }
+
+    template <typename T>
+    void Vector<T>::insert(size_type index, size_type num, const T &val)
+    {
+        reserve(size() + num);
+        size_ += num;
+        for (auto i = size() - 1; i >= index + num; --i) {
+            p_arr_[i] = p_arr_[i - num];
+        }
+        for (auto i = index; i != index + num; ++i) {
+            p_arr_[i] = val;
+        }
+    }
+
+    template <typename T>
+    void Vector<T>::remove(size_type index, size_type num)
+    {
+        check(num, "remove() on Vector that too small!");
+        size_ -= num;
+        for (auto i = index; i < size(); ++i) {
+            p_arr_[i] = p_arr_[i + num];
+        }
+    }
+
+    template <typename T>
+    void Vector<T>::reserve(size_type size)
+    {
+        if (capacity_ >= size)
+            return;
+
+        if (capacity_ < MINSIZE) capacity_ = MINSIZE;
+
+        int multiple = 2;
+        while (multiple * capacity_ < size)
+            multiple *= 2;
+        expand_capacity(multiple);
+    }
+
+    template<typename T>
+    void Vector<T>::expand_capacity(int multiple)
+    {
+        capacity_ *= multiple;
+        auto delp = p_arr_;
+        p_arr_ = new T[capacity_] {};
+        for (size_type i = 0; i != size(); ++i) {
+            p_arr_[i] = delp[i];
+        }
+        delete [] delp;
+    }
+}
+
+#endif // VECTOR_H
---
+++ test/test.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+#include "Vector"
+
+using namespace syo;
+
+static int main_ret = 0;
+static int test_count = 0;
+static int test_pass = 0;
+
+#define EXPECT_EQ_BASE(expect, actual) \
+    do {\
+        ++test_count;\
+        if (expect == actual)\
+            ++test_pass;\
+        else {\
+            std::cerr << __FILE__ << ":" << __LINE__ << ": expect: " << expect << " actual: " << actual << std::endl;\
+            main_ret = 1;\
+        }\
+    } while(0)
+
+static void test_constructor()
+{
+    Vector<int> ivec;
+    EXPECT_EQ_BASE(0, ivec.size());
+
+    ivec = Vector<int> (20, 123);
+    EXPECT_EQ_BASE(20, ivec.size());
+    for (size_t i = 0; i < ivec.size(); ++i) {
+        EXPECT_EQ_BASE(123, ivec[i]);
+        EXPECT_EQ_BASE(123, ivec.at(i));
+    }
+
+    ivec = Vector<int> (20);
+    EXPECT_EQ_BASE(20, ivec.size());
+    for (size_t i = 0; i < ivec.size(); ++i) {
+        EXPECT_EQ_BASE(0, ivec[i]);
+        EXPECT_EQ_BASE(0, ivec.at(i));
+    }
+
+    ivec = {0, 1, 2, 3, 4, 5, 6, 7 ,8 ,9 ,10, 11, 12 ,13, 14, 15, 16, 17};
+    const auto ivec2 = ivec;
+    EXPECT_EQ_BASE(18, ivec.size());
+    for (size_t i = 0; i < ivec.size(); ++i) {
+        EXPECT_EQ_BASE(static_cast<int>(i), ivec[i]);
+        EXPECT_EQ_BASE(static_cast<int>(i), ivec.at(i));
+        EXPECT_EQ_BASE(static_cast<int>(i), ivec2[i]);
+        EXPECT_EQ_BASE(static_cast<int>(i), ivec2.at(i));
+    }
+}
+
+static void test_alter()
+{
+    Vector<int> ivec;
+    for (size_t i = 0; i != 24; ++i) {
+        ivec.push_back(i);
+        EXPECT_EQ_BASE(i + 1, ivec.size());
+        EXPECT_EQ_BASE(static_cast<int>(i), ivec[i]);
+        ivec[i] *= 2;
+        EXPECT_EQ_BASE(static_cast<int>(i*2), ivec[i]);
+    }
+
+    for (size_t i = ivec.size(); i != 0; --i) {
+        EXPECT_EQ_BASE(i, ivec.size());
+        ivec.pop_back();
+    }
+
+    ivec = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    ivec.insert(4, 3, 15);
+    EXPECT_EQ_BASE(13, ivec.size());
+    for (size_t i = 4; i < 7; ++i) {
+        EXPECT_EQ_BASE(15, ivec[i]);
+    }
+
+    ivec.remove(4, 3);
+    EXPECT_EQ_BASE(10, ivec.size());
+    for (size_t i = 0; i < ivec.size(); ++i) {
+        EXPECT_EQ_BASE(static_cast<int>(i), ivec[i]);
+    }
+}
+
+static void houseword()
+{
+    using std::cout;
+    using std::endl;
+
+    Vector<int> ivec = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    cout << "Original ArrayList: \n";
+
+    cout << "Array List: ";
+    for (size_t i = 0; i < ivec.size(); ++i) {
+        cout << ivec[i] << ' ';
+    }
+    cout << endl << endl;
+
+    cout << "Insert 3 elements at index 4: \n";
+    ivec.insert(4, 3, 15);
+    cout << "Array List: ";
+    for (size_t i = 0; i < ivec.size(); ++i) {
+        cout << ivec[i] << ' ';
+    }
+    cout << endl << endl;
+
+    cout << "Remove 2 elements at index 3: \n";
+    ivec.remove(3, 2);
+    cout << "Array List: ";
+    for (size_t i = 0; i < ivec.size(); ++i) {
+        cout << ivec[i] << ' ';
+    }
+    cout << endl << endl;
+}
+
+int main()
+{
+    test_constructor();
+    test_alter();
+
+    houseword();
+
+    printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+    return main_ret;
+}
